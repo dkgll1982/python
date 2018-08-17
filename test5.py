c811ee8aa1bfe2759873335289a7c07cfde815a2
郭君 <350606539@qq.com>
2018-08-17 11:14:14 +0800

    更改注释部分
    Signed-off-by: 郭君 <350606539@qq.com>

--- test5.py
+++ test5.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*- 
-'''
- * @Author: guojun 
- * @Site: https://user.qzone.qq.com/350606539/main 
- * @Date: 2018-08-16 15:39:01 
- * @Last Modified by:   guojun 
- * @Last Modified time: 2018-08-16 15:39:01 
- * @Desc: 
-'''
+# @Author: guojun 
+# @Company: 航天神舟智慧系统技术有限公司 
+# @Site: https://user.qzone.qq.com/350606539/main 
+# @Date: 2018-08-17 11:13:55 
+# @Last Modified by: guojun 
+# @Last Modified time: 2018-08-17 11:13:55 
+# @Software: vscode 
 
 import sys
 import time;  # 引入time模块
