fbae80d2273ddd5093b6e5065031220da56683d3
郭君 <350606539@qq.com>
2018-08-18 23:26:46 +0800

    tijiao

---
+++ 爬虫/爬虫081702.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
+# @Author: guojun 
+# @Company: 航天神舟智慧系统技术有限公司 
+# @Site: https://user.qzone.qq.com/350606539/main 
+# @Date: 2018-08-17 17:56:00 
+# @Last Modified by: guojun 
+# @Last Modified time: 2018-08-17 17:56:00 
+# @Software: vscode 
+
+from urllib import request
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
