d7f6a26e8f19fa2990b3140a8d0091f2d19fbb29
郭君 <350606539@qq.com>
2018-11-14 17:07:05 +0800

    genxin
    Signed-off-by: 郭君 <350606539@qq.com>

--- restapp.py
+++ restapp.py
@@ -35,15 +35,15 @@ def signin():
     cursor.execute("SELECT table_NAME,TO_CHAR(COMMENTS) FROM user_tab_comments where COMMENTS IS NOT NULL ORDER BY TABLE_NAME")
     rows = cursor.fetchall()  # 得到所有数据集
     for row in rows:
-        str2 = str2 + row[0] + ':' + row[1] + ';'
+        str2 = str2 +'"'+ row[0] + '":"' + row[1] + '",'
 
-    str2 = str2 + "表数量: " + str(cursor.rowcount)
+    str2 = str2 + '"表数量": ' + str(cursor.rowcount)
 
     cursor.close()
     conn.close()
 
     if request.form['username'] == 'admin' and request.form['password'] == 'password':
-        return '{"data":' + str2 + '}';
+        return '{"data":{' + str2 + '}}';
     return '<h3>Bad username or password</h3>'
 
 if __name__ == '__main__':
