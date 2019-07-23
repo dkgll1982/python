c59e3fea02658a30de29d5b78c8b468c59f13126
郭君 <350606539@qq.com>
2019-07-23 15:52:48 +0800

    更新
    Signed-off-by: 郭君 <350606539@qq.com>

--- 图形绘制1-五角星.py
+++ 图形绘制1-五角星.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
+# @Author: guojun 
+# @Company: 航天神舟智慧系统技术有限公司 
+# @Site: https://user.qzone.qq.com/350606539/main 
+# @Date: 2019-07-23 15:52:33 
+# @Last Modified by: guojun 
+# @Last Modified time: 2019-07-23 15:52:33 
+# @Software: vscode 
+
 from turtle import *
 color('red', 'yellow')
 begin_fill()
