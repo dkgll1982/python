9746aa0b018a5a5c64988224d8c112f1b40b7da8
郭君 <guojun@spacesystech.com>
2020-04-07 21:18:23 +0800

    更新
    Signed-off-by: 郭君 <guojun@spacesystech.com>

---
+++ 21-网络爬虫/11-验证码识别/1.云打码_模拟登陆模拟登录古诗文网.py
@@ -0,0 +1,59 @@
+# 模拟登录古诗文网
+import requests
+from lxml import etree
+from fake_useragent import UserAgent as ua
+from ydmhttp import YDMHttp  # 导入上面整理好的云打码接口
+
+# 古诗文网址
+url = 'https://so.gushiwen.org'
+
+# 登录页面路径
+login_page_url = '/user/login.aspx?from=http://so.gushiwen.org/user/collect.aspx'
+
+# 登录请求的url
+login_request_url = '/user/login.aspx?from=http%3a%2f%2fso.gushiwen.org%2fuser%2fcollect.aspx'
+
+# 请求头
+headers = {    
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'       
+}
+
+# 准备一个session对象
+session = requests.Session()
+
+# 获取登录页面内容
+page_text = session.get(url + login_page_url, headers=headers).text
+
+# 获取保存图片验证码
+tree = etree.HTML(page_text)
+code_img_url = url + tree.xpath('//*[@id="imgCode"]/@src')[0]
+img_data = session.get(code_img_url, headers=headers).content
+filename = r'backup\爬虫\验证码\code_img.jpg'
+with open(filename, 'wb') as fp:
+    fp.write(img_data)
+ 
+# 使用云打码识别图片验证码
+ydm = YDMHttp(filename = filename, codetype = 1004, timeout = 10)
+cid, code_text = ydm.decode()
+# 以 f开头表示在字符串内支持大括号内的python 表达式
+print(f'验证码识别结果：{code_text}') 
+
+# 登录请求的参数
+# 下面两个参数会变化，所以我们就动态获取：
+__VIEWSTATE = tree.xpath('//*[@id="__VIEWSTATE"]/@value')[0]
+__VIEWSTATEGENERATOR = tree.xpath('//*[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+data = {
+    '__VIEWSTATE': __VIEWSTATE,
+    '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
+    'from': 'http://so.gushiwen.org/user/collect.aspx',
+    'email': 'www.zhangbowudi@qq.com',
+    'pwd': 'bobo328410948',  # 你没看错，它就是明文
+    'code': code_text,
+    'denglu': '登录',
+}
+
+# 开始模拟登录：
+page_text = session.post(url + login_request_url,headers=headers, data=data).text
+
+with open(r'backup\爬虫\gushiwen.html', 'w', encoding='utf-8') as fp:
+    fp.write(page_text)
---
+++ 21-网络爬虫/11-验证码识别/ydmhttp.py
@@ -0,0 +1,114 @@
+import json
+import time
+import requests 
+# 验证码类型：http://www.yundama.com/price.html
+
+class YDMHttp:
+    apiurl = 'http://api.yundama.com/api.php'
+    # 使用你自己的云打码账户：
+    username = 'dkgll'                          # 你充钱的那个用户名
+    password = 'tggcTGGC!@#$1234'               # 密码
+    appid = '10457'                             # 软件代码，开发者分成必要参数。登录开发者后台【我的软件】获得！
+    appkey = '40359b8c431f5ba11ae1c358ab6cbac3' # 通讯秘钥，开发者分成必要参数。登录开发者后台【我的软件】获得！
+
+    def __init__(self,  filename, codetype, timeout, username=None, password=None, appid=None, appkey=None):
+        """
+        :param filename: 图片路径
+        :param codetype: 验证码类型，你一定要请参考：http://www.yundama.com/price.html
+        :param timeout: 识别验证码时的超时时间（秒）
+        """
+        self.filename = filename
+        self.codetype = codetype
+        self.timeout = timeout
+        # 登录云打码
+        uid = self.login()
+        # print(f'uid: {uid}')
+        # 查询余额
+        balance = self.balance()
+        # print(f'余额：{balance}')
+
+    def request(self, fields, files=[]):
+        response = self.post_url(self.apiurl, fields, files)
+        response = json.loads(response)
+        return response
+
+    def balance(self):
+        data = {'method': 'balance', 'username': self.username, 'password': self.password,
+                'appid': self.appid, 'appkey': self.appkey}
+        response = self.request(data)
+        if (response):
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['balance']
+        else:
+            return -9001
+
+    def login(self):
+        data = {'method': 'login', 'username': self.username, 'password': self.password,
+                'appid': self.appid, 'appkey': self.appkey}
+        response = self.request(data)
+        if (response):
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['uid']
+        else:
+            return -9001
+
+    def upload(self, filename, codetype, timeout):
+        data = {'method': 'upload', 'username': self.username, 'password': self.password,
+                'appid': self.appid, 'appkey': self.appkey, 'codetype': str(codetype), 'timeout': str(timeout)}
+        file = {'file': filename}
+        response = self.request(data, file)
+        if (response):
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['cid']
+        else:
+            return -9001
+
+    def result(self, cid):
+        data = {'method': 'result', 'username': self.username, 'password': self.password,
+                'appid': self.appid, 'appkey': self.appkey, 'cid': str(cid)}
+        response = self.request(data)
+        return response and response['text'] or ''
+
+    def decode(self):
+        cid = self.upload(self.filename, self.codetype, self.timeout)
+        if (cid > 0):
+            for i in range(0, self.timeout):
+                result = self.result(cid)
+                if (result != ''):
+                    return cid, result
+                else:
+                    time.sleep(1)
+            return -3003, ''
+        else:
+            return cid, ''
+
+    def report(self, cid):
+        data = {'method': 'report', 'username': self.username, 'password': self.password,
+                'appid': self.appid, 'appkey': self.appkey, 'cid': str(cid), 'flag': '0'}
+        response = self.request(data)
+        if (response):
+            return response['ret']
+        else:
+            return -9001
+
+    def post_url(self, url, fields, files=[]):
+        for key in files:
+            files[key] = open(files[key], 'rb')
+        res = requests.post(url, files=files, data=fields)
+        return res.text
+
+
+if __name__ == '__main__':
+    # 初始化
+    ydm = YDMHttp(filename=r'backup\爬虫\验证码\code1.jpeg', codetype = '1004', timeout = 60)
+
+    # 开始识别图片验证码
+    cid, result = ydm.decode()
+    print(f'cid: {cid}', )
+    print(f'识别结果：{result}')
