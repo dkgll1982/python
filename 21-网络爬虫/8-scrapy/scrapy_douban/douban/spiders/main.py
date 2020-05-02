47c125a5044107d81da1d928ff37584a65f58a3d
郭君 <guojun@spacesystech.com>
2020-05-02 19:21:42 +0800

    更新scrapy
    Signed-off-by: 郭君 <guojun@spacesystech.com>

---
+++ 21-网络爬虫/8-scrapy/scrapy_douban/douban/spiders/main.py
@@ -0,0 +1,11 @@
+from  scrapy import cmdline
+import sys
+import os 
+
+#获取当前目录
+path = os.path.dirname(os.path.realpath(__file__))
+#设置当前目录为工作目录
+os.chdir(path)
+# 输出未过滤的页面信息
+#cmdline.execute('scrapy crawl douban_spider -o movielist.csv'.split())
+cmdline.execute('scrapy crawl douban_spider -o movielist.json'.split())
