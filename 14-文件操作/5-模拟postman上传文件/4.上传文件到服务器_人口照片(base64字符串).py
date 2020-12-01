f258492c0c2147213c3b8be70fbe2ddf743ad6c1
郭君 <guojun@spacesystech.com>
2020-12-01 18:23:27 +0800

    更新

---
+++ 14-文件操作/5-模拟postman上传文件/4.上传文件到服务器_人口照片(base64字符串).py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
+# @Author: guojun 
+# @Company: 航天神舟智慧系统技术有限公司 
+# @Site: https://user.qzone.qq.com/350606539/main 
+# @Date: 2019-08-21 16:21:20 
+# @Last Modified by: guojun 
+# @Last Modified time: 2019-08-21 16:21:20 
+# @Software: vscode 
+ 
+import cx_Oracle
+import os
+import urllib.request 
+import requests
+import json
+import os,sys,base64 
+import datetime
+from DBUtils.PooledDB import PooledDB
+
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
+#数据库连接参数/连接池
+user='cigproxy'
+password='cigproxy'
+host='10.23.179.64'
+port='15223'
+sid='orcl'
+dsn = cx_Oracle.makedsn(host,port,sid)
+pool = PooledDB(cx_Oracle,
+                mincached = 20,
+                blocking = True,
+                user = user,
+                password = password,
+                dsn = dsn)
+
+#上传地址 
+url = "http://hangzhou-sy-jczl.spacecig.com/zhzlbackend/common/uploadFile"
+#测试发现：Accept、User-Agent这俩必不可少,不行再加host
+head={
+    'Host':'hangzhou-sy-jczl.spacecig.com',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    #'Accept-Language':'zh-CN,zh;q=0.9,und;q=0.8,en;q=0.7',
+    #'Accept-Encoding':'gzip, deflate', 
+    #'Connection':'keep-alive',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    "Cookie": "passport=7690cf65-fac9-4880-b707-3a56bad528d5; CIGUserid=ADMIN; ASP.NET_SessionId=npht1wwvlgkbph24vz4mkksm; CIGServiceProvider=AE167AEEB616038DF13CB13345FEE63CA4069CDFF296A518FB2B7AF0312B5469E8D5664450A46D48B11F7C6BD5659D23CABC8207258AA8AF9D579C7CC03095BBD24BEFAB1EC450485B0D67C128614603AA55F62195D0DD83CA9BEA20A09D12AB62599226; CIGToken=de6ad04e-fc75-46f4-b4db-23d1edb6b762; CIGUsername=%E7%AE%A1%E7%90%86%E5%91%98"
+}
+
+#更新附件表
+def update_bid(cardnum,id1,id2):
+    conn = pool.connection()
+    cursor = conn.cursor()  
+    
+    sql = "update cigproxy.zz_attachment set file_type='per-image',B_ID=(select id from cigproxy.zz_person where card_num='"+cardnum+"' and rownum=1) where id = '" +id1+"' or id='"+id2+"'"
+    cursor.execute(sql) 
+    conn.commit() 
+    print("{}-上传{}照片成功！".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),cardnum))
+    cursor.close()
+    conn.close()   
+
+# 获取查询的数据列表 
+def get_data(): 
+    conn = pool.connection()
+    cursor = conn.cursor()    
+    sql1 = '''select para1 CARD_NUM,body from base_spt_xtdj_tb TA
+                JOIN zz_person TB ON TA.para1=TB.CARD_NUM AND type_id='PersonalIdentityInfo' 
+              where TB.ID NOT IN(
+                    SELECT B_ID FROM cigproxy.zz_attachment TC WHERE file_type='per-image' AND B_ID IS NOT NULL
+              ) AND LENGTH(BODY)>10 
+              AND ROWNUM<10'''  
+    cursor.execute(sql1)
+    row = cursor.fetchall()                    # 得到所有数据集
+    cursor.close() 
+    conn.close()
+    return row
+
+#base64转成本地图片
+def base64toimg(cardnum,data):    
+    with open(r"{}.jpg".format(cardnum),'wb') as f:
+        f.write(data)
+        f.close()
+
+#上传照片到服务器
+def upload_img(row):
+    cardnum = row[0]
+    #lob字转成字符串类型，否则报“TypeError: the JSON object must be str, bytes or bytearray, not LOB”的错误
+    try:
+        #zp字段值可能为空，为空则进行异常捕获
+        zp = base64.b64decode(json.loads(row[1].read())[0]['zP'])
+        
+        base64toimg(cardnum,zp)
+        
+        files = {'file': open(r"{}.jpg".format(cardnum),'rb')}
+        r = requests.post(url, files=files,headers=head)  
+        response = r.json()
+        print(response)
+        try:
+            update_bid(cardnum,response["data"]["id"],response["data"]["thumbnail"]["id"])
+        except Exception as e:
+            print("{}-上传{}照片出错，{}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),cardnum,e))
+    except Exception as e:
+        print("{}-上传{}照片出错，{}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),cardnum,e))
+    
+
+if __name__ == "__main__":   
+    rows = get_data()
+    for row in rows:
+        upload_img(row)
---
+++ 21-网络爬虫/2-requests/7.郑州基层平台_人口证照.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-  
+# @Author: suixin 
+# @Company: Aerospace Shenzhou Intelligent System Technology Co., Ltd  
+# @Site: https://user.qzone.qq.com/350606539/main 
+# @Date: 2020-12-01 15:08:14 
+# @Remark: Life is short, I use python！
+
+import requests 
+import json
+from urllib import parse
+import math 
+from threading import Thread
+import time 
+import cx_Oracle
+import time,datetime
+import hashlib
+import os
+from concurrent.futures import ThreadPoolExecutor
+import threading
+from DBUtils.PooledDB import PooledDB
+
+class ApiSpider(object):    
+    def __init__(self): 
+        super().__init__()
+        self.index = 0
+        #接口编码 
+        self.api_code = 'zxrksjhzp'                
+        self.host = 'http://172.17.217.98:8086/CSB/zzubShareService/zfw'
+        self.token_url = parse.urljoin(self.host,f'?_api_name=token&_api_version=1.0.0') 
+        os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+        user='cigproxy'
+        password='cigproxy'
+        host='172.17.217.17'
+        port='15227'
+        sid='xe'
+        dsn = cx_Oracle.makedsn(host,port,sid)
+        #连接池
+        self.pool = PooledDB(cx_Oracle,
+                             mincached = 20,
+                             blocking = True,
+                             user = user,
+                             password = password,
+                             dsn = dsn)        
+    
+    #获取token
+    def get_token(self):  
+        headers = { 
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36',
+            'Content-Length': '40',
+            'ContentType': 'application/x-www-form-urlencoded',
+            # 密钥 
+            'Authorization': 'Basic MDU3ZTdhYmMtMTg0Mi00YTZhLTg2OTYtNmI1MWQ2MmM2NmM4OkEyVjVNRFUzWlRESFlNTVRNVEcwTUkwMFlUWkhMVEcyT1RZVE5NSTFNV1EyTU1NMk5NTTQ='
+        }
+        
+        data = {
+            "grant_type":'client_credentials',
+            "scope":"service"   
+        }
+        response = requests.post(url = self.token_url, headers = headers, data = data, timeout = 30)       
+        if response.status_code == 200:
+            try:         
+                return response.json()
+            except Exception as e: 
+                print('JSON解析错误：{}'.format(e))     
+        else:
+            print('URL：{}，状态码：{}'.format(self.token_url, response.status_code)) 
+            
+    #分页查询数据        
+    def get_pagedata(self,token):      
+        # 密钥 
+        headers = {
+            'Authorization': f"bearer {token}"
+        }        
+        index = 1
+        params = {
+            '_api_name': self.api_code,
+            '_api_version': '1.0.0' 
+        }
+        
+        while True:                                
+            self.index += 1
+            print(f"正在获取第{self.index}页数据...")           
+            params["page"] = self.index 
+            response = requests.get(url = self.host,params = params, headers = headers, timeout = 10)   
+            if response.status_code == 200:
+                try:         
+                    res_data = response.json()
+                    if res_data: 
+                        data = res_data["value"]    #返回的是一个List
+                        if data:                    #不为空则表示分页数据存在，否则表示已取完  
+                            #return data
+                            li =[{"type_id":'hjrkxp',"card_num":row['zxrks_1_sfzhm'],"body":str(row)} for row in data] 
+                            self.save_data(li)               
+                        else:
+                            print(f"当前第{self.index}页已经是最后一页！")
+                            break
+                    time.sleep(0.1)                 #爬取慢一点，防止接口崩溃
+                    
+                except Exception as e: 
+                    print('JSON解析错误：{}'.format(e))     
+            else:
+                print('URL：{}，状态码：{}'.format(self.host, response.status_code))                   
+    
+    #保存数据
+    def save_data(self,data):
+        try: 
+            conn = self.pool.connection()
+            cursor = conn.cursor()      
+            #多个字典(绑定变量)
+            #params = [{"a":'100',"b":"200","c":300,"type":'test'},{"a":'1100',"b":"2200","c":3300,"type":'test'}]   
+            sql = 'INSERT INTO BASE_SPT_XTDJ_TB(type_id,para1,body) values(:type_id,:card_num,:body)'
+            cursor.executemany(sql,data)   
+            conn.commit()
+            cursor.close()
+            conn.close() 
+            print(f"保存第{self.index}页数据成功...")
+        except Exception as e:
+            print("数据库插入错误！",e)
+        
+    def start(self):    
+        res = self.get_token() 
+        if res:
+            token = res["access_token"]
+            print(f"第一步获取access_token：{token}")
+            self.get_pagedata(token)
+               
+if __name__=='__main__':  
+    start = time.time()
+    
+    api = ApiSpider()
+    api.start()
+    
+    end = time.time() 
+    print("获取接口数据完成！！！""总耗时：%0.2fs"%(end -start)); 
+    
